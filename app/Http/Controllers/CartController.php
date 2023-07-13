8464318e651149402707189c2de73b5c61ccd055
ajadoonanan <anil_jadoonanan@hotmail.com>
2023-07-12 20:04:02 -0400

    Commit 12 July 2023 8.03pm

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        return view('template0_pages/cartpage');
+    }
+
+    public function store()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\DetailsController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::view('homepage', 'template0_pages/homepage');
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,3 +27,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/store', [ProductController::class, 'index'])->name('store');
+
+Route::get('/details/{id}', [DetailsController::class, 'index'])->name('store.details');
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
