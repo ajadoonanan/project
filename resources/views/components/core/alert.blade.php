a5c5ccb213aee9d1df7928cf3d939e7dbaaddcae
ajadoonanan <anil_jadoonanan@hotmail.com>
2023-08-31 16:40:54 -0400

    alert.blade update 31 Aug 2023 4.40 pm

--- resources/views/components/core/alert.blade.php
+++ resources/views/components/core/alert.blade.php
@@ -1,6 +1,10 @@
+@if(Session::has('message'))
+
 <!-- Source: https://www.w3schools.com/bootstrap5/bootstrap_alerts.php -->
 <div class="container my-4">
     <div class="alert alert-success">
-        <strong>Success!</strong> Indicates a successful or positive action.
+        <strong>Success!</strong> {{ session('message') }}
     </div>
 </div>
+
+@endif
