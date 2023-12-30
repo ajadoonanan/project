d2c7b5d8a1379a4deef731660acae889c07c98dc
ajadoonanan <anil_jadoonanan@hotmail.com>
2023-12-30 11:26:40 -0400

    Adjust size of search message 30 Dec 2023 11.26am

--- resources/views/components/core/store-empty.blade.php
+++ resources/views/components/core/store-empty.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
 
     <div class="mt-4 p5 rounded">
-        <h1 class="text-center">Sorry, No results found</h1>
+        <h2 class="text-center">Sorry, No results were found.</h2>
     </div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+
 </div>
